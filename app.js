c86de3bc5072395da293ceec1e6ff4e9f12bf171
barringgton123 <ivangey984@gmail.com>
2026-03-25 01:43:41 +0200

    js

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict'
+//получаем информацию о class .baseinfo
+let playerinfo = document.getElementsById(".baseinfo")
+//переносим информацию из js в html
+function updateDisplay() {
+    playerinfo.innerText =
+}
+
