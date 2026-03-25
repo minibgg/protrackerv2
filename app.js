cfde62f3479a6868181e6b5e28bc7476455b6348
barringgton123 <ivangey984@gmail.com>
2026-03-25 05:42:30 +0200

    avatar

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 'use strict'
-//получаем информацию о class .baseinfo
+//получаем информацию о class
 let playerinfo = document.querySelectorAll(".baseinfo")
+//получаем информацию о id
 const input = document.getElementById("steamid")
 const player = document.getElementById("player");
 const mmrjs = document.getElementById("mmr");
 const tmmrjs = document.getElementById("tmmr");
 const wrjs = document.getElementById("wr");
-//переносим информацию из js в html
-//function updateDisplay() {
-//    playerinfo.innerText = 
-//}
+//ожидание клика и после код
 document.querySelector('.searchbtn').addEventListener('click', function(event){
   let account_id = input.value.trim()
     if (!input.value){
@@ -18,7 +16,7 @@ document.querySelector('.searchbtn').addEventListener('click', function(event){
 
     fetch (`https://api.opendota.com/api/players/${account_id}`)
     .then(response => response.json()) // Конвертация в JSON
-    .then(data => {
+    .then(data => { //только тут работа с данными
         player.innerText = data.profile.personaname;
         mmrjs.innerText = data.computed_mmr;
         if (data.computed_mmr_turbo !== null) {
@@ -26,10 +24,13 @@ document.querySelector('.searchbtn').addEventListener('click', function(event){
         } else {
           tmmrjs.innerText = "нет игр в турбо"  
         }
+        const img = document.createElement('img');
+          img.src = data.profile.avatarfull; // Установка пути из JSON
+        document.getElementById('steamavatar').appendChild(img);
     });
     fetch (`https://api.opendota.com/api/players/${account_id}/wl`)
-    .then(response => response.json())
-    .then(data => {
+    .then(response => response.json()) //конвертация в JSON
+    .then(data => { //только тут работа с данными
       if (data.win + data.lose == 0) {
         wrjs.innerText = "игры не найдены";
       } else {
@@ -38,5 +39,3 @@ document.querySelector('.searchbtn').addEventListener('click', function(event){
     });
 
 })
-
-873568882
