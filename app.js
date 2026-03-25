9c8f6cd98262cc8a24d762db7e00c86df2bcfd39
barringgton123 <ivangey984@gmail.com>
2026-03-25 03:03:07 +0200

    mmr tmmr player

--- app.js
+++ app.js
@@ -2,20 +2,29 @@
 //получаем информацию о class .baseinfo
 let playerinfo = document.querySelectorAll(".baseinfo")
 const input = document.getElementById("steamid")
+const player = document.getElementById("player");
+const mmrjs = document.getElementById("mmr");
+const tmmrjs = document.getElementById("tmmr");
+const wrjs = document.getElementById("wr");
 //переносим информацию из js в html
 //function updateDisplay() {
 //    playerinfo.innerText = 
 //}
 document.querySelector('.searchbtn').addEventListener('click', function(event){
   let account_id = input.value.trim()
+    if (!input.value){
+       alert("нерпавельный Id")
+    }
+
     fetch (`https://api.opendota.com/api/players/${account_id}`)
     .then(response => response.json()) // Конвертация в JSON
-    .then(data => console.log(data));
-      if (!input.value){
-        alert("нерпавельный Id")
-      }
-      if (!data) {
-        alert(123)
-      }
+    .then(data => {
+        player.innerText = data.profile.personaname;
+        mmrjs.innerText = data.computed_mmr;
+        tmmrjs.innerText = data.computed_mmr_turbo;
+        
+    });
+
 })
 
+873568882
