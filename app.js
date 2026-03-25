e836babb389d7ad0185029dc50b57cb89655e284
barringgton123 <ivangey984@gmail.com>
2026-03-25 02:31:07 +0200

    api into json check id

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 'use strict'
 //получаем информацию о class .baseinfo
 let playerinfo = document.querySelectorAll(".baseinfo")
+const input = document.getElementById("steamid")
 //переносим информацию из js в html
 //function updateDisplay() {
 //    playerinfo.innerText = 
 //}
 document.querySelector('.searchbtn').addEventListener('click', function(event){
-    console.log ('123')
-//    fetch (`https://api.opendota.com/api/players/${account_id}`)
+  let account_id = input.value.trim()
+    fetch (`https://api.opendota.com/api/players/${account_id}`)
+    .then(response => response.json()) // Конвертация в JSON
+    .then(data => console.log(data));
+      if (!input.value){
+        alert("нерпавельный Id")
+      }
+      if (!data) {
+        alert(123)
+      }
 })
 
