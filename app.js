2904dd09fb93a840b229b4983fb35a7b7912162a
barringgton123 <ivangey984@gmail.com>
2026-03-25 02:02:41 +0200

    search button

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 'use strict'
 //получаем информацию о class .baseinfo
-let playerinfo = document.getElementsById(".baseinfo")
+let playerinfo = document.querySelectorAll(".baseinfo")
 //переносим информацию из js в html
-function updateDisplay() {
-    playerinfo.innerText =
-}
+//function updateDisplay() {
+//    playerinfo.innerText = 
+//}
+document.querySelector('.searchbtn').addEventListener('click', function(event){
+    console.log ('123')
+//    fetch (`https://api.opendota.com/api/players/${account_id}`)
+})
 
