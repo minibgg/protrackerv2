bb706ef05ca8594f7d3d72fa127db731466e3548
minibgg <ivangey984@gmail.com>
2026-03-31 02:50:41 +0300

    heroes name

--- match.js
+++ match.js
@@ -1,15 +1,19 @@
 'use strict'
 
-const matchDetails = document.getElementById('matchDetails');
+    const matchDetails = document.getElementById('matchDetails');
 
-function getMatchIdFromUrl() {
+function getMatchIdFromUrl() {//разобратся подробнее
     const params = new URLSearchParams(window.location.search);
     return params.get('id');
 }
 
 async function loadMatchDetails() {
-    const matchId = getMatchIdFromUrl();
+    const matchId = getMatchIdFromUrl();//до
 
+        if (!matchId) {
+        matchDetails.innerText = "ID матча не найден в URL";//не нашло игру
+        return;
+    }
 
     const dotaApi = {
     async getMatchInfo(matchId){
@@ -17,15 +21,33 @@ async function loadMatchDetails() {
     const data = await res.json();
     return data;
   },
+    async getHeroes(){
+  const res = await fetch(`https://api.opendota.com/api/heroes`);
+  const heroesName = await res.json();
+  return heroesName;
 }
-
+}
+    
 try{
-    const data = await dotaApi.getMatchInfo(matchId)
+    const matchData = await dotaApi.getMatchInfo(matchId)
+    const heroes = await dotaApi.getHeroes()
 
-    matchDetails.innerText = `ID матча: ${data.match_id}`;
+            const players = matchData.players;
+
+        const heroesList = players.map(player => {
+            const hero = heroes.find(h => h.id === player.hero_id);
+            return hero ? hero.localized_name : `Неизвестный герой (id: ${player.hero_id})`;
+        });
+
+    matchDetails.innerHTML = `
+            <h2>ID матча: ${matchData.match_id}</h2>
+            <ul>
+                ${heroesList.map(name => `<li>${name}</li>`).join('')}
+            </ul>
+    `;
 
 } catch(error){
-    matchDetails.innerText = "не удалось загрузить матч"
+    matchDetails.innerText = "не удалось загрузить матч"//поиск багов
 }
 }
-loadMatchDetails();
+loadMatchDetails();//вызов функции
